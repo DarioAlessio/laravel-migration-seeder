487750fd90c08e53666c192d4ca774642447074f
Dario <darioalessio79@gmail.com>
2021-01-28 18:02:57 +0100

    push

---
+++ laravel/database/factories/LocationFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Location;
+use Faker\Generator as Faker;
+
+$factory->define(Location::class, function (Faker $faker) {
+    return [
+        'name' => $faker -> firstName,
+        'street '=> $faker-> address,
+        'number_employees'=>  rand(1,50),
+    ];
+});
---
+++ laravel/database/migrations/2021_01_28_161658_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name',60);
+            $table->string('street',60);
+            $table->tinyInteger('number_employees');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
